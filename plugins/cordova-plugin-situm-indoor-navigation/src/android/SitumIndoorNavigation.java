86a2c5413066dc0e2e4588dc7c4c22d48134c5e3
ignisit <ignisit@ignisits-Mac-mini.local>
2017-05-04 20:03:49 +0530

    Added buildings method

--- plugins/cordova-plugin-situm-indoor-navigation/src/android/SitumIndoorNavigation.java
+++ plugins/cordova-plugin-situm-indoor-navigation/src/android/SitumIndoorNavigation.java
@@ -1,6 +1,9 @@
 package cordova.plugin.situm.indoor.navigation;
 
-import es.situm.sdk.SitumSdk;
+
+import android.util.Log;
+
+import com.ionicframework.demoapp675353.MainActivity;
 
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
@@ -11,11 +14,22 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Collection;
+
+import es.situm.sdk.SitumSdk;
+import es.situm.sdk.error.Error;
+import es.situm.sdk.model.cartography.Building;
+import es.situm.sdk.utils.Handler;
+
 /**
  * This class echoes a string called from JavaScript.
  */
 public class SitumIndoorNavigation extends CordovaPlugin {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
+    // The building identifier where positioning will start.
+    // You can see your buildings IDs and names running this example in the Logcat
+    private static final String BUILDING_ID = "YOUR_BUILDING_ID";
 
     /**
      * Sets the context of the Command. This can then be used to do things like
@@ -32,19 +46,43 @@ public class SitumIndoorNavigation extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
+        if (action.equals("fetchBuildings")) {
             String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+            this.fetchBuildings(message, callbackContext);
             return true;
         }
         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
+    private void fetchBuildings(String message, CallbackContext callbackContext) {
+        final CallbackContext cbc = callbackContext;
+
+        //Get all the buildings of the account
+        SitumSdk.communicationManager().fetchBuildings(new Handler<Collection<Building>>() {
+            @Override
+            public void onSuccess(Collection<Building> buildings) {
+                Log.d(TAG, "onSuccess: Your buildings: ");
+                for (Building building : buildings) {
+                    Log.i(TAG, "onSuccess: " + building.getIdentifier() + " - " + building.getName());
+
+                    if (BUILDING_ID.equals(building.getIdentifier())) {
+//                        selectedBuilding = building;
+                    }
+                }
+
+                if (buildings.isEmpty()) {
+                    Log.e(TAG, "onSuccess: you have no buildings. Create one in the Dashboard");
+                    return;
+                } else {
+                    cbc.success(buildings.toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Error error) {
+                Log.e(TAG, "onFailure:" + error);
+                cbc.error("Expected one non-empty string argument.");
+            }
+        });
     }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+declare var window: any;
 
 @Component({
   selector: 'page-home',
@@ -8,7 +9,14 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
+  	setTimeout(function() {
+  		console.log("Plugin Call");
+  		window.plugins.SitumIndoorNavigation.fetchBuildings("test", function(res){
+  			console.log("RESPONSE "+res);
+  		}, function(error){
+  			console.log("Error "+error);
+  		});
+  	}, 5000);
   }
 
 }
