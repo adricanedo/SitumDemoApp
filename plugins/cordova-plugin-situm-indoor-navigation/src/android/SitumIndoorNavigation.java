d2cf675bfea351d9d0f5348b6d048f2e710c8ba3
ignisit <ignisit@ignisits-Mac-mini.local>
2017-05-04 14:31:31 +0530

    Initial Commit

---
+++ plugins/cordova-plugin-situm-indoor-navigation/src/android/SitumIndoorNavigation.java
@@ -0,0 +1,50 @@
+package cordova.plugin.situm.indoor.navigation;
+
+import es.situm.sdk.SitumSdk;
+
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.apache.cordova.CordovaWebView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class SitumIndoorNavigation extends CordovaPlugin {
+
+
+    /**
+     * Sets the context of the Command. This can then be used to do things like
+     * get file paths associated with the Activity.
+     *
+     * @param cordova The context of the main Activity.
+     * @param webView The CordovaWebView Cordova is running in.
+     */
+    @Override
+    public void initialize(final CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        SitumSdk.init(cordova.getActivity().getApplicationContext());
+    }
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("coolMethod")) {
+            String message = args.getString(0);
+            this.coolMethod(message, callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    private void coolMethod(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+}
